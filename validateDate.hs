dfd3b76a4f7ff18440913c511b5a56410be6938e
Nalydia0 <dylanlhession@gmail.com>
2024-10-13 12:17:41 +0100

    Wrote dateParser and comments

--- validateDate.hs
+++ validateDate.hs
@@ -1,8 +1,12 @@
-
 --Create a function that parses the date,
 -- This will take in the string format "dd/mm/yyyy" and convert it,
 -- into the format day month year 
 dateParser :: String -> [Int]
-dataParser "" = [] --Base case where an empty string = an empty list
-
-
+dateParser "" = [] --Base case where an empty string = an empty list
+dateParser string = 
+    --Read the first result before the comma and conveert to an int
+    -- Then prepend this with the recursive call of this function
+     --rest of string represents the rest of the string
+    read input : dateParser restOfString --Read the first result before the comma and conveert to an int
+    where -- This will split the string at the '/'
+    (input, restOfString) = break (== '/') string
