513a32f58965bb07b808d7020e542a5f138aceff
Nalydia0 <dylanlhession@gmail.com>
2024-10-13 12:00:53 +0100

    Writing Type Signatures

--- validateDate.hs
+++ validateDate.hs
@@ -1,1 +1,8 @@
--- Create a definition to validate the date
+
+--Create a function that parses the date,
+-- This will take in the string format "dd/mm/yyyy" and convert it,
+-- into the format day month year 
+dateParser :: String -> [Int]
+dataParser "" = [] --Base case where an empty string = an empty list
+
+
